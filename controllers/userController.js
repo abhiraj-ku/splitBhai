172a40c5b321baea40643359af8073c3b9fedb76
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-08-19 23:36:24 +0530

    add disposable email checker json file and code logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,14 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const speakeasy = require("speakeasy");
+const isDisposableEmail = require("../utils/checkDisposableEmail");
+
+module.exports.register = async (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name && !email) {
+    console.log("bhen ke laude bhag ja");
+  }
+  if (isDisposableEmail(email)) {
+  }
+};
