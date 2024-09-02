d3bb43b2b994123e7c6527f544c95548a87e7387
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-09-02 19:14:19 +0530

    add todo to work on

---
+++ src/controllers/groupController.js
@@ -0,0 +1,7 @@
+const groupModel = require("../models/groupModel");
+
+// TODO: implement the create group func wrt to frontend design
+module.exports.createGroup = async (req, res) => {};
+
+// TODO: implement the join group func wrt to frontend design
+module.exports.joinGroup = async (req, res) => {};
